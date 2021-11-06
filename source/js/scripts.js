c6126aa9b296b6f808e8f8d2f872aa78f46e3719
victoria-nsl <vbnaish@yandex.ru>
2021-11-06 10:44:03 +0300

    Добавляет плавные переходы, корректирует js код

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,121 +1,40 @@
 'use strict';
-const imagesSlide = document.querySelectorAll(".countries__slider-item");
-const buttonsSlide = document.querySelectorAll(".countries__slider-button");
-const linksSlide = document.querySelectorAll(".places-visit__link");
+const body = document.body;
 
-const overlayNavigationMain = document.querySelector(".page-header__overlay");
-const navigationMain = document.querySelector(".main-navigation");
-const navigationToggle = navigationMain.querySelector(".main-navigation__toggle");
+const overlayNavigationMain = document.querySelector('.page-header__overlay');
+const navigationMain = document.querySelector('.main-navigation');
+const navigationToggle = navigationMain.querySelector('.main-navigation__toggle');
 const linksNavigationMain = navigationMain.querySelectorAll('.main-navigation__link');
 
-const sliderListCountries = document.querySelector(".countries__slider-list");
-const pricesList = document.querySelector(".prices__list");
+const linksSlide = document.querySelectorAll('.places-visit__link');
 
-const body = document.body;
-const overlayPopup = document.querySelector(".modal__overlay");
+const buttonsSlide = document.querySelectorAll('.countries__slider-button');
+const sliderListCountries = document.querySelector('.countries__slider-list');
+const imagesSlide = sliderListCountries.querySelectorAll('.countries__slider-item');
 
-const popupBuy = document.querySelector(".modal-buy__card");
-const buttonPopupBuyClose = popupBuy.querySelector(".modal-buy__close");
+const pricesList = document.querySelector('.prices__list');
 
-const popupSuccess = document.querySelector(".modal-success__card");
-const buttonPopupSuccessClose = popupSuccess.querySelector(".modal-success__close");
+const formQuestions = document.querySelector('.questions__form');
+const inputTelQuestions= formQuestions.querySelector('.questions__input--tel');
+const inputEmailQuestions= formQuestions.querySelector('.questions__input--email');
 
-const formBuy = popupBuy.querySelector(".modal-buy__form");
-const inputTelModal= formBuy.querySelector(".modal-buy__input--tel");
-const inputEmailModal= formBuy.querySelector(".modal-buy__input--email");
+const overlayPopupBuy = document.querySelector('.modal-buy');
+const popupBuy = overlayPopupBuy.querySelector('.modal-buy__card');
+const buttonPopupBuyClose = popupBuy.querySelector('.modal-buy__close');
 
-const formQuestions = document.querySelector(".questions__form");
-const inputTelQuestions= formQuestions.querySelector(".questions__input--tel");
-const inputEmailQuestions= formQuestions.querySelector(".questions__input--email");
+const overlayPopupSuccess = document.querySelector('.modal-success');
+const popupSuccess = overlayPopupSuccess.querySelector('.modal-success__card');
+const buttonPopupSuccessClose = popupSuccess.querySelector('.modal-success__close');
 
-const buttonWatchTour = document.querySelector(".button--travel-europe");
-
-/*==========ПЕРЕКЛЮЧЕНИЕ СЛАЙДОВ /ТАБОВ/==============*/
-
-sliderListCountries.classList.remove("countries__slider-list--nojs");
-
-const changeSlide = (imageSlide, buttonSlide) => {
-
-  imagesSlide.forEach((slide) => {
-    if (slide.classList.contains("countries__slider-item--current")) {
-      slide.classList.remove("countries__slider-item--current");
-    }
-  });
-
-  buttonsSlide.forEach((button) => {
-    if (button.classList.contains("countries__slider-button--current")) {
-      button.classList.remove("countries__slider-button--current");
-    }
-  });
-
-  imageSlide.classList.add("countries__slider-item--current");
-  buttonSlide.classList.add("countries__slider-button--current");
-};
+const formBuy = popupBuy.querySelector('.modal-buy__form');
+const inputTelModal= formBuy.querySelector('.modal-buy__input--tel');
+const inputEmailModal= formBuy.querySelector('.modal-buy__input--email');
 
 
-buttonsSlide.forEach((buttonSlide,index) => {
-  buttonSlide.addEventListener("click", () => {
-    changeSlide(imagesSlide[index],buttonSlide);
-  });
-});
+const buttonWatchTour = document.querySelector('.button--travel-europe');
+const buttonsBuy = document.querySelectorAll('[data-buy]');
 
-
-/*=============ПЕРЕКЛЮЧЕНИЕ НА СЛАЙДЫ /ТАБЫ/ ПРИ НАЖАТИИ НА ССЫЛКИ В РАЗДЕЛЕ СТРАНЫ ============*/
-
-const showSlide = (imageSlide, linkSlide) => {
-  imagesSlide.forEach((image) => {
-    if (image.classList.contains("countries__slider-item--current")) {
-      image.classList.remove("countries__slider-item--current");
-    }
-  });
-
-  linksSlide.forEach((link) => {
-    if (link.classList.contains("places-visit__link--curent")) {
-      link.classList.remove("places-visit__link--curent");
-    }
-  });
-
-  imageSlide.classList.add("countries__slider-item--current");
-  linkSlide.classList.add("places-visit__link--curent");
-};
-
-linksSlide.forEach((linkSlide,index) => {
-  linkSlide.addEventListener("click", () => {
-    showSlide(imagesSlide[index],linkSlide);
-  });
-});
-
-
-/*==============ОТКРЫТИЕ МЕНЮ /НАВИГАЦИИ/ В ПЛАНШЕТНОЙ И ДЕСКТОПНОЙ ВЕРСИИИ ================*/
-
-navigationMain.classList.remove("main-navigation--nojs");
-
-
-const closeMenu = () => {
-  navigationMain.classList.add('main-navigation--closed');
-  navigationMain.classList.remove('main-navigation--opened');
-  overlayNavigationMain.classList.remove('page-header__overlay--show');
-  body.classList.remove('page-body--no-scroll');
-};
-
-const openMenu = () => {
-  navigationMain.classList.remove('main-navigation--closed');
-  navigationMain.classList.add('main-navigation--opened');
-  overlayNavigationMain.classList.add('page-header__overlay--show');
-  body.classList.add('page-body--no-scroll');
-};
-
-navigationToggle.addEventListener("click", () => {
-  if (navigationMain.classList.contains("main-navigation--closed")) {
-    openMenu();
-    return
-  }
-  closeMenu();
-});
-
-
-//Плавная прокрутка
-
+/*==============ПЛАВНАЯ ПРОКРУТКА================*/
 const scrollSmoothly = (anchorLink) => {
   const blockId = anchorLink.getAttribute('href').substring(1);
 
@@ -141,92 +60,150 @@ buttonWatchTour.addEventListener('click', (evt) => {
 });
 
 
+/*==============ОТКРЫТИЕ МЕНЮ /НАВИГАЦИИ/ В ПЛАНШЕТНОЙ И ДЕСКТОПНОЙ ВЕРСИИИ ================*/
+navigationMain.classList.remove('main-navigation--nojs');
+
+
+const closeMenu = () => {
+  navigationMain.classList.add('main-navigation--closed');
+  navigationMain.classList.remove('main-navigation--opened');
+  overlayNavigationMain.classList.remove('page-header__overlay--show');
+  body.classList.remove('page-body--no-scroll');
+};
+
+const openMenu = () => {
+  navigationMain.classList.remove('main-navigation--closed');
+  navigationMain.classList.add('main-navigation--opened');
+  overlayNavigationMain.classList.add('page-header__overlay--show');
+  body.classList.add('page-body--no-scroll');
+};
+
+navigationToggle.addEventListener('click', () => {
+  if (navigationMain.classList.contains('main-navigation--closed')) {
+    openMenu();
+    return
+  }
+  closeMenu();
+});
+
+/*=============ПЕРЕКЛЮЧЕНИЕ НА СЛАЙДЫ /ТАБЫ/ ПРИ НАЖАТИИ НА ССЫЛКИ В РАЗДЕЛЕ СТРАНЫ ============*/
+
+const showSlide = (imageSlide, linkSlide) => {
+  imagesSlide.forEach((image) => {
+    if (image.classList.contains('countries__slider-item--current')) {
+      image.classList.remove('countries__slider-item--current');
+    }
+  });
+
+  linksSlide.forEach((link) => {
+    if (link.classList.contains('places-visit__link--curent')) {
+      link.classList.remove('places-visit__link--curent');
+    }
+  });
+
+  imageSlide.classList.add('countries__slider-item--current');
+  linkSlide.classList.add('places-visit__link--curent');
+  scrollSmoothly(linkSlide);
+};
+
+linksSlide.forEach((linkSlide,index) => {
+  linkSlide.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showSlide(imagesSlide[index],linkSlide);
+  });
+});
+
+
+/*==========ПЕРЕКЛЮЧЕНИЕ СЛАЙДОВ /ТАБОВ/==============*/
+
+sliderListCountries.classList.remove('countries__slider-list--nojs');
+
+const changeSlide = (imageSlide, buttonSlide) => {
+
+  imagesSlide.forEach((slide) => {
+    if (slide.classList.contains('countries__slider-item--current')) {
+      slide.classList.remove('countries__slider-item--current');
+    }
+  });
+
+  buttonsSlide.forEach((button) => {
+    if (button.classList.contains('countries__slider-button--current')) {
+      button.classList.remove('countries__slider-button--current');
+    }
+  });
+
+  imageSlide.classList.add('countries__slider-item--current');
+  buttonSlide.classList.add('countries__slider-button--current');
+};
+
+
+buttonsSlide.forEach((buttonSlide,index) => {
+  buttonSlide.addEventListener('click', () => {
+    changeSlide(imagesSlide[index],buttonSlide);
+  });
+});
+
+
 
 /*======ПРОВЕРКА LocalStorage============*/
 let isStorageSupport = true;
-let storageTel = "";
-let storageEmail = "";
+let storageTel = '';
+let storageEmail = '';
 
-try {
-  storageTel = localStorage.getItem("tel");
-  storageEmail = localStorage.getItem("email");
-
-} catch (err) {
+const setItemLocalStorage = (phone, email) => {
+  try {
+    storageTel = localStorage.getItem('tel');
+    storageEmail = localStorage.getItem('email');
+  } catch (err) {
     isStorageSupport = false;
-}
+  }
 
-/*========ОТКРЫТИЕ ФОРМЫ "КУПИТЬ ТУР"==========*/
-
-const onButtonOpenClick = (evt) => {
-  if(evt.target.matches("a")) {
-    evt.preventDefault();
-    popupBuy.classList.add("modal-buy__show");
-    overlayPopup.classList.add("modal__show");
-    body.classList.add("page-body--no-scroll");
-
-    if(storageTel && storageEmail) {
-      inputTelModal.value = storageTel;
-      inputEmailModal.value = storageEmail;
-      inputTelModal.focus();
-    } else {
-      inputTelModal.focus();
-    }
+  if (storageTel || storageEmail) {
+    phone.value = storageTel || '';
+    email.value = storageEmail || '' ;
   }
 };
 
-sliderListCountries.addEventListener("click",onButtonOpenClick);
-pricesList.addEventListener("click",onButtonOpenClick);
+setItemLocalStorage (inputTelQuestions, inputEmailQuestions);
 
-/*=========ОТПРАВКА ФОРМ==============*/
 
-formBuy.addEventListener('submit', (evt)  => {
-  if (!inputTelModal.value) {
+/*========ОТКРЫТИЕ ФОРМЫ "КУПИТЬ ТУР"==========*/
+const openPopup = () => {
+  popupBuy.classList.add('modal-buy__show-card');
+  overlayPopupBuy.classList.add('modal-buy__show-overlay');
+  body.classList.add('page-body--no-scroll');
+  inputTelModal.focus();
+  setItemLocalStorage (inputTelModal, inputEmailModal);
+};
+
+buttonsBuy.forEach((buttonBuy) => {
+  buttonBuy.addEventListener('click', (evt) => {
     evt.preventDefault();
-  } else {
-
-    if(isStorageSupport) {
-      localStorage.setItem("tel",  inputTelModal.value);
-      localStorage.setItem("email", inputEmailModal.value);
-    }
-
-    popupSuccess.classList.add("modal-success__show");
-    popupBuy.classList.remove("modal-buy__show");
-    body.classList.add("page-body--no-scroll");
-  }
-});
-
-
-formQuestions.addEventListener('submit', (evt)  => {
-  if (!inputTelQuestions.value) {
-    evt.preventDefault();
-  } else {
-
-    if(isStorageSupport) {
-      localStorage.setItem("tel",  inputTelQuestions.value);
-      localStorage.setItem("email", inputEmailQuestions.value);
-    }
-
-    popupSuccess.classList.add("modal-success__show");
-    overlayPopup.classList.add("modal__show");
-    body.classList.add("page-body--no-scroll");
-  }
+    openPopup();
+  });
 });
 
 /*================ЗАКРЫТИЕ МОДАЛЬНЫХ ОКОН========================*/
-
 const closePopup = () => {
-  if (popupBuy.classList.contains("modal-buy__show")) {
-    popupBuy.classList.remove("modal-buy__show");
+  if (popupBuy.classList.contains('modal-buy__show-card')) {
+    popupBuy.classList.remove('modal-buy__show-card');
   }
 
-  if (popupSuccess.classList.contains("modal-success__show")) {
-    popupSuccess.classList.remove("modal-success__show");
+  if (popupSuccess.classList.contains('modal-success__show-card')) {
+    popupSuccess.classList.remove('modal-success__show-card');
   }
 
-  if (overlayPopup.classList.contains("modal__show")) {
-    overlayPopup.classList.remove("modal__show");
-    body.classList.remove("page-body--no-scroll");
+  if (overlayPopupBuy.classList.contains('modal-buy__show-overlay')) {
+    overlayPopupBuy.classList.remove('modal-buy__show-overlay');
+    body.classList.remove('page-body--no-scroll');
   }
+
+  if (overlayPopupSuccess.classList.contains('modal-success__show-overlay')) {
+    overlayPopupSuccess.classList.remove('modal-success__show-overlay');
+    body.classList.remove('page-body--no-scroll');
+  }
+
+  setItemLocalStorage (inputTelQuestions, inputEmailQuestions);
 };
 
 //закрытие окoн "КУПИТЬ ТУР", "ФОРМА ОТПРАВЛЕНА УСПЕШНО" через кнопку
@@ -234,24 +211,60 @@ const onButtonCloseClick = (evt) => {
   evt.preventDefault();
   closePopup();
 }
-buttonPopupBuyClose.addEventListener("click", onButtonCloseClick);
-buttonPopupSuccessClose.addEventListener("click", onButtonCloseClick);
+buttonPopupBuyClose.addEventListener('click', onButtonCloseClick);
+buttonPopupSuccessClose.addEventListener('click', onButtonCloseClick);
 
 //закрытие окон "КУПИТЬ ТУР", "ФОРМА ОТПРАВЛЕНА УСПЕШНО" через Esc
-
 const onDocumentEscKeydown = (evt) => {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     closePopup();
   }
 };
-document.addEventListener("keydown", onDocumentEscKeydown);
+document.addEventListener('keydown', onDocumentEscKeydown);
 
 
 //закрытие окон "КУПИТЬ ТУР", "ФОРМА ОТПРАВЛЕНА УСПЕШНО" через overlay
+
 const onOverlayClick = (evt) => {
-  evt.preventDefault();
-  closePopup();
+  if (evt.target.matches('section')) { //останавливает погружение
+    evt.stopPropagation(); //останавливает всплытие
+    closePopup();
+  }
 };
 
-overlayPopup.addEventListener("click", onOverlayClick);
+overlayPopupBuy.addEventListener('click', onOverlayClick);
+overlayPopupSuccess.addEventListener('click', onOverlayClick);
+
+/*=========ОТПРАВКА ФОРМ==============*/
+
+formQuestions.addEventListener('submit', (evt)  => {
+  if (!inputTelQuestions.value) {
+    evt.preventDefault();
+  } else {
+    popupSuccess.classList.add('modal-success__show-card');
+    overlayPopupSuccess.classList.add('modal-success__show-overlay');
+    body.classList.add('page-body--no-scroll');
+
+    if(isStorageSupport) {
+      localStorage.setItem('tel', inputTelQuestions.value);
+      localStorage.setItem('email', inputEmailQuestions.value);
+    }
+  }
+});
+
+formBuy.addEventListener('submit', (evt)  => {
+  if (!inputTelModal.value) {
+    evt.preventDefault();
+  } else {
+    popupSuccess.classList.add('modal-success__show-card');
+    overlayPopupSuccess.classList.add('modal-success__show-overlay');
+    popupBuy.classList.remove('modal-buy__show-card');
+    overlayPopupBuy.classList.remove('modal-buy__show-overlay');
+
+    if(isStorageSupport) {
+      localStorage.setItem('tel', inputTelModal.value);
+      localStorage.setItem('email',  inputEmailModal.value);
+    }
+  }
+});
